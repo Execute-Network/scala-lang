70d513fe9b8923a7774681d1cb14634f8cb31a00
swiesend <sebastian@wiesendahl.de>
2015-03-10 18:13:36 -0600

    Hello World!

---
+++ src/main/scala/helloworld/HelloApp.scala
@@ -0,0 +1,5 @@
+package helloworld
+
+object HelloApp extends App {
+  println("Hello World!");
+}
---
+++ src/main/scala/helloworld/HelloMain.scala
@@ -0,0 +1,7 @@
+package helloworld
+
+object HelloMain {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!");
+  }
+}
